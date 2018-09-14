d04f8e9639dc0227af3f6499c94d9c29b88f98b7
Jeromy Van Dusen <JVanDusen@obsglobal.com>
2018-09-14 09:25:55 -0500

    Document deploy method.

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,3 +1,12 @@
+// The following environment variables must have values before calling this method:
+// - IMAGE_TAG
+// - DISCOVERY_PORT
+// - ADMIN_PORT
+// - WEB_PORT
+// - GREETING_PORT
+// - MONGODB_DATA_PATH
+// - MONGODB_PORT
+// - NETWORK_NAME
 def call(String swarmManager, String stackName) {
-    sh "docker -H ${swarmManager} stack deploy --compose-file docker-compose.yml --resolve-image always --prune ${stackName}"
+    sh "docker -H $swarmManager stack deploy --compose-file docker-compose.yml --resolve-image always --prune $stackName"
 }
