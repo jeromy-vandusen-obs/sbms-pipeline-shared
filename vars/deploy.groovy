ba476551f4553760f9456271c1b9f3ed2543fd3e
Jeromy Van Dusen <jvandusen@obsglobal.com>
2018-09-17 11:55:30 -0500

    Remove comment that is specific to a particular compose file.

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,12 +1,3 @@
-// The following environment variables must have values before calling this method:
-// - IMAGE_TAG
-// - DISCOVERY_PORT
-// - ADMIN_PORT
-// - WEB_PORT
-// - GREETING_PORT
-// - MONGODB_DATA_PATH
-// - MONGODB_PORT
-// - NETWORK_NAME
 def call(String stackName) {
     sh "docker stack deploy --compose-file docker-compose.yml --resolve-image always --prune $stackName"
 }
