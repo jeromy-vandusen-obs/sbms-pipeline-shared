9b665c89764bc304bb63dfea328374c52b5776db
Jeromy Van Dusen <jvandusen@obsglobal.com>
2018-09-19 14:37:20 -0500

    Add waitUntilActive method.

---
+++ vars/waitUntilActive.groovy
@@ -0,0 +1,14 @@
+boolean call(String host, String port, int pingInterval, int maxPings) {
+    for (int index = 0; index < maxPings; index ++) {
+        sh "curl -s http://$host:$port/dependencyHealth | python -c \"import sys, json; print json.load(sys.stdin)['overallStatus']\" > status.txt || true"
+        String status = readFile('status.txt').trim()
+        if (status == 'UP') {
+            echo "Running the tests now..."
+            return true
+        }
+        else {
+            sh "sleep $pingInterval"
+        }
+    }
+    return false
+}
