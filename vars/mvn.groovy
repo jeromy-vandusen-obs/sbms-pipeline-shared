16134624f4040b540bf9ca609e093a6cebadc2f3
Jeromy Van Dusen <jvandusen@obsglobal.com>
2018-09-21 10:15:49 -0500

    Simplify mvn method.

--- vars/mvn.groovy
+++ vars/mvn.groovy
@@ -1,21 +1,4 @@
-def call(String goals, boolean skipUnitTests = false, boolean skipIntegrationTests = false) {
+def call(String args) {
     sh "chmod +x mvnw"
-    if (skipUnitTests || skipIntegrationTests) {
-        String args
-        if (skipUnitTests) {
-            if (skipIntegrationTests) {
-                args = "-DskipTests"
-            }
-            else {
-                args = "-DskipUnitTests"
-            }
-        }
-        else {
-            args = "-DskipIntegrationTests"
-        }
-        sh "./mvnw $args $goals"
-    }
-    else {
-        sh "./mvnw $goals"
-    }
+    sh "./mvnw $args"
 }
