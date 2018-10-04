3dc3a55066b9a56c13e2b3e211d2cc50e931f220
Jeromy Van Dusen <jvandusen@obsglobal.com>
2018-10-04 15:46:04 -0500

    Fix method and variable names.

--- vars/promoteAndUpdateRunningImage.groovy
+++ vars/promoteAndUpdateRunningImage.groovy
@@ -1,5 +1,5 @@
 def call(String imageName, String sourceImageTag, String imageTag, String serviceName, String registryCredentialsId = 'DOCKER_HUB_CREDENTIALS', String registryUrl = '') {
-    sh "docker pull $imageNae:$sourceImageTag"
+    sh "docker pull $imageName:$sourceImageTag"
     sh "docker tag $imageName:$sourceImageTag $imageName:$imageTag"
     withDockerRegistry([url: registryUrl, credentialsId: registryCredentialsId]) {
         sh "docker push $imageName:$imageTag"
