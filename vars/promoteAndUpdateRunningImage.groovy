56be105255fa729094303376110ba9a66f6387ba
Jeromy Van Dusen <jvandusen@obsglobal.com>
2018-10-05 08:44:32 -0500

    Add TLS configuration to method.

--- vars/promoteAndUpdateRunningImage.groovy
+++ vars/promoteAndUpdateRunningImage.groovy
@@ -1,8 +1,10 @@
-def call(String imageName, String sourceImageTag, String imageTag, String serviceName, String registryCredentialsId = 'DOCKER_HUB_CREDENTIALS', String registryUrl = '') {
-    sh "docker pull $imageName:$sourceImageTag"
-    sh "docker tag $imageName:$sourceImageTag $imageName:$imageTag"
-    withDockerRegistry([url: registryUrl, credentialsId: registryCredentialsId]) {
-        sh "docker push $imageName:$imageTag"
+def call(String host, String imageName, String sourceImageTag, String imageTag, String serviceName, String registryCredentialsId = 'DOCKER_HUB_CREDENTIALS', String registryUrl = '') {
+    withEnv(["DOCKER_HOST=$host", "DOCKER_TLS_VERIFY=1", "DOCKER_CERT_PATH=/root/.tls"]) {
+        sh "docker pull $imageName:$sourceImageTag"
+        sh "docker tag $imageName:$sourceImageTag $imageName:$imageTag"
+        withDockerRegistry([url: registryUrl, credentialsId: registryCredentialsId]) {
+            sh "docker push $imageName:$imageTag"
+        }
+        sh "docker service update --image $imageName:$imageTag $serviceName"
     }
-    sh "docker service update --image $imageName:$imageTag $serviceName"
 }
